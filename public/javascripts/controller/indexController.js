6917de3b17ab2cb14756a752663ee308503d4871
engKaya <36958369+engKaya@users.noreply.github.com>
2020-09-11 00:11:22 +0300

    kullanıcı connect ve dsicoonect eventleri

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,13 +1,57 @@
 app.controller('indexController', ['$scope','indexFactory' ,($scope,indexFactory) => {
-    
-    const connectionOptions = {
-        reconnectionAttempts:3,
-        reconnectionDelay:600,
+
+    $scope.messages = []
+
+    $scope.init = () =>{
+        const username = prompt('Enter username: ');
+
+        if (username)
+            initSocket(username);
+        else
+           return false
     }
-    
-    indexFactory.connectSocket('http://localhost:3000',connectionOptions).then(socket =>{
-        console.log('Bağlandı',socket)
-    }).catch(err =>{
-        console.log(err)
-    });
+
+    function initSocket(username) {
+
+        const connectionOptions = {
+            reconnectionAttempts:3,
+            reconnectionDelay:600,
+        }
+
+        indexFactory.connectSocket('http://localhost:3000',connectionOptions)
+        .then((socket) =>{
+           socket.emit('newUser',{username})
+
+           socket.on('newUser',data=>{
+               const messageData = {
+                   type: {
+                       code:0, //server veya client mesajı belirtir
+                       messageType:1 //giriş veya çıkış msajı olduğunu belirtir
+                   },
+                   username:data.username,
+               }
+
+               $scope.messages.push(messageData)
+               $scope.$apply()
+           })
+
+            socket.on('disUser',(user)=>{
+                const messageData = {
+                    type: {
+                        code:0,
+                        messageType:0
+                    },
+                    username:user.username,
+                }
+
+                $scope.messages.push(messageData)
+                $scope.$apply()
+            })
+
+
+        }).catch(err =>{
+            console.log(err)
+        });
+
+    }
 }])
--- src/socketApi.js
+++ src/socketApi.js
@@ -2,11 +2,38 @@ const socketio = require('socket.io')
 const io = socketio()
 
 const socketApi = { }
-
 socketApi.io = io;
 
-io.on('connection',(socket)=>{
+const users = { };
+
+io.on('connection',(socket)=>   {
     console.log('connection')
+
+    socket.on('newUser',(data)=>{
+        const defaultdata={
+            id: socket.id,
+            position:{
+                x:0,
+                y:0,
+            },
+        }
+
+        const userData = Object.assign(defaultdata,data)
+        users[socket.id] = userData
+
+        socket.broadcast.emit('newUser',users[socket.id])
+        console.log(users)
+
+    })
+
+    socket.on('disconnect',()=>{
+
+        socket.broadcast.emit('disUser',users[socket.id])
+        delete users[socket.id]
+        console.log(users)
+    })
+
+
 })
 
 module.exports =socketApi
