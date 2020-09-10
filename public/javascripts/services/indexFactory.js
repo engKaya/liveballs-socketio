8e1e5129530668ae477fa7cce893e2f74841d3d5
engKaya <36958369+engKaya@users.noreply.github.com>
2020-09-10 20:42:14 +0300

    index controller - index factory

---
+++ public/javascripts/services/indexFactory.js
@@ -0,0 +1,13 @@
+app.factory('indexFactory',[()=>{
+    const connectSocket = (url,options) =>
+        new Promise((resolve, reject) => {
+        const socket = io.connect(url, options);
+        socket.on('connect',()=>{
+            resolve(socket);
+        })
+        socket.on('connect_error',()=>{
+            reject(new Error('connect error'));
+        })
+    })
+    return {connectSocket}
+}])
